e7f32ff6bc048b311f6bf737b5c741ec88325df8
Florent Messa <florent.messa@gmail.com>
2018-06-11 21:26:52 +0200

    Update build-and-push-to-gh-pages.bash

--- build-and-push-to-gh-pages.bash
+++ build-and-push-to-gh-pages.bash
@@ -4,7 +4,7 @@ rm -fr public
 git worktree add -B gh-pages public origin/gh-pages
 hugo
 git -C public add --all
-git config user.email "yann.salaun@ulule.com"
-git config user.name "Yann Salaün"
+git config user.email "tech@ulule.com"
+git config user.name "Tech @ Ulule"
 git -C public commit -m "Publishing to gh-pages" || exit 0
 git push origin gh-pages
